d0b3093037252c06f3c45ee9f8adcd5f11be17e3
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2022-01-30 14:47:33 +0400

    Leafing page via cycle and Jsoup

--- src/main/java/bonalex/App.java
+++ src/main/java/bonalex/App.java
@@ -10,32 +10,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
+    public static final String URL = "https://4pda.to/";
+
     public static void main(String[] args) throws IOException {
+        parsePosts().forEach(System.out::println);
+    }
+
+    private static List<Post> parsePosts() throws IOException {
         List<Post> posts = new ArrayList<>();
-        System.out.println("Connecting to main page ");
-        Document doc = Jsoup.connect("https://4pda.to/").get();
-        Elements postTitleElements = doc.getElementsByAttributeValue("itemprop", "url");
+        for (int i = 1; i < 7; i++) {
+            System.out.println("Connecting to " + i + " page ");
+            Document doc = Jsoup.connect(i == 1 ? URL : URL + "/page/" + i + "/").get();
+            Elements postTitleElements = doc.getElementsByAttributeValue("itemprop", "url");
 
-        for (Element postTitleElement : postTitleElements) {
-            String detailsLink = postTitleElement.attr("href");
-            Post post = new Post();
-            post.setDetailLink(detailsLink);
-            post.setTitle(postTitleElement.attr("title"));
-            System.out.println("Connecting to post: " + detailsLink);
-            Document postDetailsDoc = Jsoup.connect(detailsLink).get();
-            try {
-                Element authorElement = postDetailsDoc.getElementsByClass("name").first().child(0);
-                post.setAuthor(authorElement.text());
-                post.setDetailLink(authorElement.attr("href"));
-                post.setDateOfCreated(postDetailsDoc.getElementsByClass("date").first().text());
-            } catch (NullPointerException e) {
-                post.setAuthor("Author not determinated");
-                post.setAuthorDetailsLink("No link");
-                post.setDateOfCreated("No date");
+            for (Element postTitleElement : postTitleElements) {
+                String detailsLink = postTitleElement.attr("href");
+                Post post = new Post();
+                post.setDetailLink(detailsLink);
+                post.setTitle(postTitleElement.attr("title"));
+                System.out.println("Connecting to post: " + detailsLink);
+                Document postDetailsDoc = Jsoup.connect(detailsLink).get();
+                try {
+                    Element authorElement = postDetailsDoc.getElementsByClass("name").first().child(0);
+                    post.setAuthor(authorElement.text());
+                    post.setDetailLink(URL + authorElement.attr("href"));
+                    post.setDateOfCreated(postDetailsDoc.getElementsByClass("date").first().text());
+                } catch (NullPointerException e) {
+                    post.setAuthor("Author not determinated");
+                    post.setAuthorDetailsLink("No link");
+                    post.setDateOfCreated("No date");
+                }
+                posts.add(post);
             }
-            posts.add(post);
         }
-
-        posts.forEach(System.out::println);
+        return posts;
     }
 }
